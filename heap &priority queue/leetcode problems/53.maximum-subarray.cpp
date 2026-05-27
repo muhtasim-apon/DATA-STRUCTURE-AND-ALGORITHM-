164bcb48a89011159192b00110e440bd9db73454
muhtasim-apon <syedmuhtasimapon@gmail.com>
2026-05-27 12:29:29 +0600

    kadane algo uploaded

---
+++ heap &priority queue/leetcode problems/53.maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans=INT_MIN;
+        int sum=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum+=nums[i];
+            if(ans<sum)ans=sum;
+            if(sum<0)sum=0;
+        }
+        return ans;
+
+    }
+};
